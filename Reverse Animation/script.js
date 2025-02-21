d63bdf455d53328abc79008e9efb53ed3249f7ff
𝐀𝐌𝐀𝐍 𝐏𝐀𝐍𝐃𝐄𝐘. <69503838+aman-ap-official@users.noreply.github.com>
2025-02-21 20:17:46 +0530

    Add files via upload

---
+++ Reverse Animation/script.js
@@ -0,0 +1,8 @@
+gsap.to("#box",{
+    x:1000, // for x axis
+    duration:2, // for speed
+    delay:1, // for delay
+    rotate:360, // for rotation
+    repeat: -1, // for repeat
+    yoyo:true, // for reverse animation
+})
